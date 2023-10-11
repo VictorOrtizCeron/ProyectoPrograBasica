0593d96f6c25fd44f60ac945e83e7ebe27ae37dc
Bmow1402 <pmendez80528@ufide.ac.cr>
2023-10-10 20:03:25 -0600

    Esqueleto de menu principal

--- src/grupo7proyectofinalsc202/Grupo7ProyectoFinalSC202.java
+++ src/grupo7proyectofinalsc202/Grupo7ProyectoFinalSC202.java
@@ -1,20 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package grupo7proyectofinalsc202;
-
-/**
- *
- * @author victo
- */
+import javax.swing.JOptionPane;
 public class Grupo7ProyectoFinalSC202 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        // Definicion de variables.
+        
+        int opcion = 0;
+        int opcionRegistro = 0;
+        boolean repetirMenu = true;
+        boolean repetirMenuRegistro = true;
+
+        //Menu principal 
+        while (repetirMenu == true) {
+            String menuPrincipal = "Bienvenid@ a supermercados Los Pollitos\n\n1-Registro.\n2-Ver inventario.\n3-Registrar una venta.\n4-Salir.\n ";
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menuPrincipal));
+            switch (opcion) {
+                case 1: //Menu de registro
+                    while (repetirMenuRegistro) {
+                        String menuRegistro = "Haz seleccionado registro\n\n1-Registrar nuevos productos.\n2-Registrar una marca.\n3-Registrar una categoria.\n4-Regresar\n";
+                        opcionRegistro = Integer.parseInt(JOptionPane.showInputDialog(menuRegistro));
+                        switch (opcionRegistro) { //Sub menu de registro
+                            case 1: // Registrar nuevos productos.
+
+                                break;
+
+                            case 2: // Registrar una marca.
+
+                                break;
+
+                            case 3: //• Registrar una categoría.
+
+                                break;
+
+                            case 4: //Regresar
+                                repetirMenuRegistro = false;
+                                JOptionPane.showMessageDialog(null, "Volviendo al menu principal...");
+                                break;
+
+                            default: //Opcion invalida 
+                                JOptionPane.showMessageDialog(null, "Ups! opcion invalida, intentalo de nuevo.");
+
+                        }
+                    }
+                    break;
+
+                case 2: //Ver Inventario 
+                    break;
+
+                case 3: //Registrar una venta 
+                    break;
+
+                case 4: //Salir del menu principal
+                    repetirMenu = false;
+                    JOptionPane.showMessageDialog(null, "Hasta luego, cerrando sistema.");
+                    break;
+
+                default: //Opcion invalida 
+                    JOptionPane.showMessageDialog(null, "Lo siento, opcion invalida, intentalo de nuevo.");
+
+            }
+        }
+
+    }
+
 }
