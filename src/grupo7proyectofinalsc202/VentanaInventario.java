955f3735bf1b6d4dceffb07e4f7b1a3860255121
Victor Ortiz <victorandresortiz@gmail.com>
2023-11-12 17:56:58 -0600

    agregado el método concatenar inventario que muestra todo el inventario en ventana de inventario

--- src/grupo7proyectofinalsc202/VentanaInventario.java
+++ src/grupo7proyectofinalsc202/VentanaInventario.java
@@ -3,35 +3,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package grupo7proyectofinalsc202;
-
+ 
 import javax.swing.JOptionPane;
 
 public class VentanaInventario {
+    Producto frijol = new Producto(200, "Frijoles Ducal", "Comida", "Ducal", "5nov2024", false);
 
-    private String inventario;//Van a ser arreglos
-    private String Categorias;
-    private String Marcas;
+
+    private Producto[] inventario = {frijol};//Van a ser arreglos
+    private String[] Categorias;
+    private String[] Marcas;
 
     public void start() {
         int opcionInventario = 0;
+
+       
+        inventario[0] = frijol;
+        
         boolean repetirMenuInventario = true;
         while (repetirMenuInventario) {
             String menuInventario = "Haz seleccionado Ver inventario\n\n"
-                    + "1.filtrar por categoria.\n"
-                    + "2.Mostrar inventario.\n"
-                    + "3.Regresar.\n";
+                    + concatenarInventario(this.inventario)+"\n"
+                    + "1.Filtrar por categoría.\n"
+                    + "2.Regresar.\n";
 
             opcionInventario = Integer.parseInt(JOptionPane.showInputDialog(menuInventario));
             switch (opcionInventario) { //Sub menu de registro
-                case 1: // FiltrarPorCartegoria.
+                case 1: // FiltrarPorCategoria.
 
+                    String input = JOptionPane.showInputDialog(null, "Escoja una marca");
                     break;
 
-                case 2: // MostrarInventario.
-
-                    break;
-
-                case 3: //• Regresar.
+                case 2: //• Regresar.
 
                     repetirMenuInventario = false;
                     JOptionPane.showMessageDialog(null, "Volviendo al menu principal...");
@@ -45,29 +48,42 @@ public class VentanaInventario {
         }
     }
 
-    public String getInventario() {
+    public String concatenarInventario(Producto[] inventario) {
+        String inventarioString = "";
+
+        if (inventario.length != 0) {
+            for (int i = 0; i < inventario.length; i++) {
+
+                inventarioString = inventarioString + "\n " + inventario[i].getNombre();
+            }
+        } else {
+            inventarioString = "Inventario vacío";
+        }
+        return inventarioString;
+    }
+
+    public Producto[] getInventario() {
         return inventario;
     }
 
-    public void setInventario(String inventario) {
+    public void setInventario(Producto[] inventario) {
         this.inventario = inventario;
     }
 
-    public String getCategorias() {
+    public String[] getCategorias() {
         return Categorias;
     }
 
-    public void setCategorias(String Categorias) {
+    public void setCategorias(String[] Categorias) {
         this.Categorias = Categorias;
     }
 
-    public String getMarcas() {
+    public String[] getMarcas() {
         return Marcas;
     }
 
-    public void setMarcas(String Marcas) {
+    public void setMarcas(String[] Marcas) {
         this.Marcas = Marcas;
     }
-    
 
 }
